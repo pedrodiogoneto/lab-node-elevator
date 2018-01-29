e4bb68694e5b78d69a036dea6ff0edfdbf8a4d36
Pedro Neto <pedro.diogo.neto@outlook.pt>
2018-01-29 20:57:38 +0100

    iterations 1 and 2 complete. 3 almost there

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -3,17 +3,40 @@ class Elevator {
     this.floor      = 0;
     this.MAXFLOOR   = 10;
     this.requests   = [];
+    this.direction = 'up';
   }
 
-  start() { }
-  stop() { }
-  update() { }
-  _passengersEnter() { }
-  _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+  start() {const timeoutId = setInterval(this.update(), 1000)}
+
+  stop() {clearInterval(this.timeoutId)}
+  
+  update() {this.log()}
+  
+  // _passengersEnter() { }
+  
+  // _passengersLeave() { }
+  
+  floorUp() {
+    if (this.floor < 10 && this.direction === 'up') {
+      this.floor += 1;
+      this.direction = 'up';
+    }
+    this.update();
+  }
+  
+  floorDown() { 
+    if (this.floor > 0 && this.direction === 'down') {
+      this.floor -= 1;
+      this.direction = 'down';
+    }
+    this.update();
+  }
+
+  // call() { }
+  
+  log() { 
+    console.log("Direction: " + this.direction + " | Floor " + this.floor);
+  }
 }
 
 module.exports = Elevator;
--- starter_code/person.js
+++ starter_code/person.js
@@ -1,5 +1,12 @@
 class Person {
   constructor(name, originFloor, destinationFloor){
+    this.name = name;
+    this.originFloor = originFloor;
+    this.destinationFloor = destinationFloor;
+  }
+
+  call (name1, originFloor1, destinationFloor1) {
+    this.elevator.requests.push([name1, originFloor1, destinationFloor1]);
   }
 }
 
